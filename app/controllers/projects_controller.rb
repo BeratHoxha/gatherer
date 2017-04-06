dfd867ec13a9ce04da6aef917f474c1a74aacf92
Berat Hoxha <berathoxha4@gmail.com>
2017-04-06 17:19:00 +0200

    Add controller actions + views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-
+    @projects = Project.all
   end
 
   def new 
@@ -8,6 +8,23 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @action = CreatesProject.new(
+      name: params[:project][:name],
+      task_string: params[:project][:tasks],
+    )
+    success = @action.create
+    
+    if success
+      redirect_to projects_path
+    else 
+      @project = action.project
+      render :new
+    end
+  end
 
+  private 
+
+  def project_params
+    params.require(:project).permit(:name, :task_string)
   end
 end
--- spec/actions/creates_project_spec.rb
+++ spec/actions/creates_project_spec.rb
@@ -4,7 +4,8 @@ describe "task string parsing" do
   let(:creator) { CreatesProject.new(name: "Test", task_string: task_string) }
   let(:tasks) { creator.convert_string_to_tasks }
 
-  describe "with an empty string" do let(:task_string) { "" }
+  describe "with an empty string" do
+    let(:task_string) { "" }
     specify { expect(tasks.size).to eq(0) }
   end
 
@@ -20,7 +21,7 @@ describe "task string parsing" do
     specify { expect(tasks.size).to eq(1) }
     specify { expect(tasks.map(&:title)).to eq(["Start things"]) }
     specify { expect(tasks.map(&:size)).to eq([3]) }
-  end
+ end
 
   describe "with multiple tasks" do
     let(:task_string) { "Start things:3\nEnd things:2" }
